d34e7cb401fc967fcf9d72c48388e1a0ca65c971
oivindbakke <oivind.bakke95@gmail.com>
2019-07-17 16:26:42 +0200

    Kommentare eingefügt

--- Abgabe/Code/test.m
+++ Abgabe/Code/test.m
@@ -13,13 +13,13 @@ classdef test < matlab.unittest.TestCase
     end
     methods (Test)
         function test_toolboxes(testCase)
-            testCase.verifyFalse(check_toolboxes('challenge.m'));
-            testCase.verifyFalse(check_toolboxes('disparity_map.m'));
+            testCase.verifyFalse(check_toolboxes('challenge.m')); % check_toolboxes returns true if more than just standard matlab are used.
+            testCase.verifyFalse(check_toolboxes('disparity_map.m')); % Tests will be considered failed, if not false
             testCase.verifyFalse(check_toolboxes('verify_dmap.m'));
         end
-        function test_variables(testCase)%,group_number, members, mail, elapsed_time, D,R,T,p)
-            load('challenge.mat');
-            testCase.verifyNotEmpty(members);
+        function test_variables(testCase)%Loads the .mat file, which has been saved right before calling the test function.
+            load('challenge.mat');		 %Verify that the specified variables are not empty or that they are greater than 0
+            testCase.verifyNotEmpty(members); 
             testCase.verifyNotEmpty(mail);
             testCase.verifyEqual(group_number,59);
             testCase.verifyGreaterThan(elapsed_time,0);
@@ -28,11 +28,10 @@ classdef test < matlab.unittest.TestCase
             testCase.verifyGreaterThan(T,0);
             testCase.verifyGreaterThan(p,0);            
         %%Tests the PSNR of our function to the one of Image Processing Toolbox
-        %check if the results are within a certain tolerance. (D,G tolerance
-        %are to be specified.
+        %check if the results are within a certain tolerance(10^-6). 
             p_ours=verify_dmap(D,G);
             peaksnr=psnr(D,G);
-            testCase.verifyEqual(p_ours,peaksnr,'AbsTol',e^-6);
+            testCase.verifyEqual(p_ours,peaksnr,'AbsTol',e^-6); %
         end
     end
 end
